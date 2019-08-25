99b02996d0a71c43a855aef7617db4848c8d195c
Pedro Amaral <pedropaulo@life.com.br>
2019-08-25 14:42:43 -0300

    Migrate

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'pedro1991',
+  database: 'Meetapp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
