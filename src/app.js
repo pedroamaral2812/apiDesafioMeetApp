337bd174e4e51d7ff448a906d612c23c4efcbc04
Pedro Amaral <pedropaulo@life.com.br>
2019-08-25 14:16:23 -0300

    Eslint funcionando

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import routes from './routes';
 
 class App {
@@ -7,10 +7,9 @@ class App {
 
     this.middlewares();
     this.routes();
-    
   }
 
-  middlewares(){
+  middlewares() {
     this.server.use(express.json());
   }
 
